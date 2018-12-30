a63ed51ee739a6e2e96fb549ad0f1e757cfee48d
DeepLearning VM <google-dl-platform@googlegroups.com>
2018-12-30 23:34:12 +0000

    No progress

--- code/commuter.py
+++ code/commuter.py
@@ -13,6 +13,15 @@ def predict_journeys(learner,dataset):
     accuracy=result/dataset.shape[0]
     return(accuracy)
 
+def predict_journey(learner,detectedActivity,geoHash,minuteOfday,weekday):
+    data = np.array([['','detectedActivity','geoHash','minuteOfDay','weekday'],
+                ["row1",detectedActivity,geoHash,minuteOfday,weekday]])            
+    dr=pd.DataFrame(data=data[1:,1:],
+                    index=data[1:,0],
+                    columns=data[0,1:]).astype(np.int64)
+    predicted = learner.predict(dr.iloc[0])
+    return(predicted[0],str(round(predicted[2].max().item(),2)))
+
 def make_shure_we_got_enough_rows(dataset,minrows=1000):
     "If the dataset has fewer rows that minrows, whole dataset copies will be added at the end until at least minrows exists"
     newset = pd.DataFrame()
